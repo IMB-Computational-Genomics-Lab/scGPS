a7f07efe515665b6ce05b8251be4fe186719c884
Quan <quanaibn@gmail.com>
2018-11-14 12:19:44 +1000

    add summarise function for network plot

--- R/scgps_prediction_summary.R
+++ R/scgps_prediction_summary.R
@@ -96,7 +96,7 @@ summary_deviance <- function(object = LSOLDA_dat) {
 
 
 summary_prediction_lasso <- function(LSOLDA_dat = NULL, nPredSubpop = NULL) {
-    pred_lasso <- LSOLDA_dat$LassoPredict
+    pred_lasso <- LSOLDA_dat$ElasticNetPredict
     for (i in 1:length(pred_lasso)) {
         for (j in 1:length(pred_lasso[[i]])) {
             if (identical(pred_lasso[[i]][[j]], numeric(0))) {
@@ -173,6 +173,50 @@ summary_prediction_lda <- function(LSOLDA_dat = NULL, nPredSubpop = NULL) {
     pred_lda_mtrx$names <- row_names
 
     return(pred_lda_mtrx)
+}
+
+
+#' @name reformat_LASSO
+#' @title summarise bootstrap runs for Lasso model, from \code{n} bootstraps
+#' @description the training and prediction results from \code{bootstrap_scGPS} were written to
+#' the object \code{LSOLDA_dat}, the \code{reformat_LASSO} summarises prediction
+#' for \code{n} bootstrap runs
+#' @param c_selectID  is the original cluster to be projected
+#' @param mp_selectID  is the target mixedpop to project to
+#' @param LSOLDA_dat is the results from the bootstrap_scGPS
+#' @param nPredSubpop is the number of clusters in the target mixedpop \code{row_cluster <-length(unique(target_cluster))}
+#' @return a dataframe containg information for the Lasso prediction results, each column
+#' contains prediction results for all subpopulations from each bootstrap run
+#' @export
+#' @examples
+#' c_selectID<-1
+#' day2 <- sample1
+#' mixedpop1 <-NewscGPS(ExpressionMatrix = day2$dat2_counts, GeneMetadata = day2$dat2geneInfo,
+#'                     CellMetadata = day2$dat2_clusters)
+#' day5 <- sample2
+#' mixedpop2 <-NewscGPS(ExpressionMatrix = day5$dat5_counts, GeneMetadata = day5$dat5geneInfo,
+#'                     CellMetadata = day5$dat5_clusters)
+#' genes <-GeneList
+#' genes <-genes$Merged_unique
+#' LSOLDA_dat <- bootstrap_scGPS(nboots = 2,mixedpop1 = mixedpop1, mixedpop2 = mixedpop2, genes=genes, c_selectID, listData =list(), cluster_mixedpop1 = colData(mixedpop1)[,1], cluster_mixedpop2=colData(mixedpop2)[,1])
+#' reformat_LASSO(LSOLDA_dat=LSOLDA_dat, nPredSubpop=length(unique(colData(mixedpop2)[,1])), c_selectID = 1, mp_selectID =2)
+#'
+reformat_LASSO <-function(c_selectID = NULL, mp_selectID = NULL, LSOLDA_dat = NULL, 
+                          nPredSubpop = row_cluster, Nodes_group = "#7570b3", nboots=2){
+  LASSO_out <- summary_prediction_lasso(LSOLDA_dat=LSOLDA_dat, nPredSubpop = nPredSubpop)
+  LASSO_out <-as.data.frame(LASSO_out)
+  temp_name <- gsub("ElasticNet for subpop", "C", LASSO_out$names)
+  temp_name <- gsub(" in target mixedpop", "_MP", temp_name)
+  LASSO_out$names <-temp_name
+  source <-rep(paste0("C",c_selectID,"_MP",mp_selectID), length(temp_name))
+  LASSO_out$Source <- source
+  LASSO_out$Node <- source
+  LASSO_out$Nodes_group <- rep(Nodes_group, length(temp_name))
+  colnames(LASSO_out) <-c(paste0("Boostrap", 1:nboots), "Target", "Source", "Node", "NodeGroup")
+  matrx_mean <- apply(LASSO_out[,c(1:nboots)],1,function(x){mean(as.numeric(x))})
+  
+  LASSO_out$Value <- matrx_mean
+  return(LASSO_out)
 }
 
 
